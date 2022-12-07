ff60bbe40a9d6a1d6f5732634ebdf4b61ab31dce
kvnochieng52 <kvnochieng52@gmail.com>
2022-12-07 12:53:39 +0300

    react install complete

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        \App\Models\User::factory(10)->create([
+            'name' => 'Kevin O',
+            'email' => 'kvnochieng52@gmail.com',
+            'password' => \bcrypt('password'),
+        ]);
     }
 }
